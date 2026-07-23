1a9e1047360c07f0e6357f788353bd80fd848784
asundar-mags <asundar@students.mags.school.nz>
2026-07-24 11:45:53 +1200

    New Bug Fixes and Updated the Earth Map

--- the-grimstone/scripts/global.gd
+++ the-grimstone/scripts/global.gd
@@ -1,6 +1,8 @@
 extends Node
 
 @export var exits: Array[Node]
+var spawn: Vector2
+var change_spawn: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- the-grimstone/scripts/player.gd
+++ the-grimstone/scripts/player.gd
@@ -2,8 +2,19 @@ extends CharacterBody2D
 
 @export var player: CharacterBody2D
 
+var neutral_level: String = "neutral"
+var initial: int = 0
+var second: int = 1
+var third: int = 2
+var fourth: int = 3
+
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+
+func _ready() -> void:
+	if Global.change_spawn and get_parent().name == neutral_level:
+		global_position = Global.spawn
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,8 +25,6 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,24 +32,27 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-
+func _spike_take_damage(body: CharacterBody2D) -> void:
+	get_tree().call_deferred("reload_current_scene")
+	
 func _fire_map_entry(body: CharacterBody2D) -> void:
+	Global.spawn = Global.exits[initial].global_position
 	get_tree().change_scene_to_file("res://scenes/firemap.tscn")
-
 
 func _air_map_entry(body: CharacterBody2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/airmap.tscn")
-
 
 func _water_map_entry(body: CharacterBody2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/watermap.tscn")
 
 func _earth_map_entry(body: CharacterBody2D) -> void:
+	Global.spawn = Global.exits[third].global_position
 	get_tree().change_scene_to_file("res://scenes/earthmap.tscn")
 
 func _fire_map_exit(body: CharacterBody2D) -> void:
+	Global.change_spawn = true
 	get_tree().change_scene_to_file("res://scenes/neutral level.tscn")
 
-func _spike_take_damage(body: CharacterBody2D) -> void:
-	get_tree().call_deferred("reload_current_scene")
+func _earth_map_exit(body: CharacterBody2D) -> void:
+	Global.change_spawn = true
+	get_tree().change_scene_to_file("res://scenes/neutral level.tscn")
